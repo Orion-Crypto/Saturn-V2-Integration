030ef606b08b230aaad2fc802687ae8ae5af2d20
Christopher Maselli <Christopherfmaselli@gmail.com>
2023-11-28 00:16:22 -0500

    Adding the preprod api url back

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,8 @@ import { getAPIKey, getJWTToken } from '@/api/authentication';
 import { GraphQLClient } from 'graphql-request';
 
 //export const v2GraphQLClient = new GraphQLClient(process.env.NEXT_PUBLIC_V2_GRAPHQL_API_URL as string);
-//export const v2GraphQLClient = new GraphQLClient("https://api.preprod.saturnnft.io/graphql/");
-export const v2GraphQLClient = new GraphQLClient("https://localhost:5001/graphql/");
+export const v2GraphQLClient = new GraphQLClient("https://api.preprod.saturnnft.io/graphql/");
+//export const v2GraphQLClient = new GraphQLClient("https://localhost:5001/graphql/");
 //export const v2GraphQLClient = new GraphQLClient("https://api.saturnnft.io/graphql/");
 
 

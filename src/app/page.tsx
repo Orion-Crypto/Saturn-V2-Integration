9ff6b7ba862eebaf843ea505d140f437cd438f20
Nicholas Maselli <maselli.nicholas@gmail.com>
2023-12-01 22:28:54 -0500

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 const Home = () => {
-    return <></>;
+    return (
+        <>
+            <div className="p-24 text-white">
+                <div className="drop-shadow-black-sharp h-12 w-40 cursor-pointer rounded-lg bg-blue-500 p-4"></div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -1,7 +1,19 @@
+import { WalletButton } from '@/components/Elements/Buttons/WalletButtons/WalletButton';
+
 export const Navbar = () => {
     return (
         <>
-            <div className="drop-shadow-black-sharp flex w-full bg-space-900 text-white">Hello World</div>
+            <div className="h-20"></div>
+            <div className="drop-shadow-black-sharp fixed top-0 flex h-20 w-full items-center bg-space-900 px-4 text-white">
+                <div>Hello</div>
+                <div>Hello</div>
+                <div>Hello</div>
+                <div>Hello</div>
+                <div className="grow"></div>
+                <div>
+                    <WalletButton />
+                </div>
+            </div>
         </>
     );
 };
